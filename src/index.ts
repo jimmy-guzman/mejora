714dac7f3481b990794f157b563aa306b13bfc96
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-12-29 16:50:52 -0600

    feat: ✨ add shorter typescript and eslint aliases (#10)
    🏁 Closes #3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-export { eslintCheck } from "./checks/eslint";
-export { typescriptCheck } from "./checks/typescript";
+export { eslintCheck as eslint, eslintCheck } from "./checks/eslint";
+export {
+  typescriptCheck as typescript,
+  typescriptCheck,
+} from "./checks/typescript";
 export { defineConfig } from "./config";
 export type { Config } from "./types";
