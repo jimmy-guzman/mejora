b6528de7dc9db8970be3bdd4f9268b2b865d76a0
jimmy-guzman <hi@jimmy.codes>
2025-12-30 13:35:47 -0600

    fix: 🐛 heading should be surrounded by blank lines
    🏁 Closes #27

--- src/reports.spec.ts
+++ src/reports.spec.ts
@@ -43,7 +43,9 @@ describe("generateMarkdownReport", () => {
 
       ## eslint (1 issue)
 
+
       ### [src/index.ts](../src/index.ts) (1)
+
       - [Line 10](../src/index.ts#L10) - 'foo' is never used
 
       "
@@ -68,7 +70,9 @@ describe("generateMarkdownReport", () => {
 
       ## typescript (1 issue)
 
+
       ### [src/types.ts](../src/types.ts) (1)
+
       - [src/types.ts](../src/types.ts) - Type error
 
       "
@@ -97,13 +101,17 @@ describe("generateMarkdownReport", () => {
 
       ## eslint (1 issue)
 
+
       ### [src/a.ts](../src/a.ts) (1)
+
       - [Line 1](../src/a.ts#L1) - error a
 
 
       ## typescript (1 issue)
 
+
       ### [src/b.ts](../src/b.ts) (1)
+
       - [Line 2](../src/b.ts#L2) - error b
 
       "
@@ -145,12 +153,15 @@ describe("generateMarkdownReport", () => {
 
       ## eslint (2 issues)
 
+
       ### [src/valid.ts](../src/valid.ts) (1)
+
       - [Line 10](../src/valid.ts#L10) - valid error
 
-      ### Other Issues (1)
-      -  - invalid item without path
 
+      ### Other Issues (1)
+
+      -  - invalid item without path
       "
     `);
   });
@@ -172,7 +183,9 @@ describe("generateMarkdownReport", () => {
 
       ## eslint (1 issue)
 
+
       ### [src/index.ts](../src/index.ts) (1)
+
       - [Line 10](../src/index.ts#L10)
 
       "
@@ -201,11 +214,15 @@ describe("generateMarkdownReport", () => {
 
       ## eslint (3 issues)
 
+
       ### [src/index.ts](../src/index.ts) (2)
+
       - [Line 10](../src/index.ts#L10) - error 1
       - [Line 20](../src/index.ts#L20) - error 2
 
+
       ### [src/utils.ts](../src/utils.ts) (1)
+
       - [Line 5](../src/utils.ts#L5) - error 3
 
       "
@@ -235,7 +252,9 @@ describe("generateMarkdownReport", () => {
 
       ## eslint (1 issue)
 
+
       ### [C](../C) (1)
+
       - [Line \project\src\index.ts](../C#L\project\src\index.ts) - 'foo' is never used
 
       "
@@ -265,7 +284,9 @@ describe("generateMarkdownReport", () => {
 
       ## typescript (1 issue)
 
+
       ### [D](../D) (1)
+
       - [Line \code\app\types.ts](../D#L\code\app\types.ts) - Type error
 
       "
@@ -299,7 +320,9 @@ describe("generateMarkdownReport", () => {
 
       ## eslint (3 issues)
 
+
       ### [C](../C) (3)
+
       - [Line \project\src\index.ts](../C#L\project\src\index.ts) - error 1
       - [Line \project\src\index.ts](../C#L\project\src\index.ts) - error 2
       - [Line \project\src\utils.ts](../C#L\project\src\utils.ts) - error 3
